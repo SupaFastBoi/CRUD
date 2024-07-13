af07a6633066a06951204e606a3ed76ede138263
SupaFastBoi <gheocastefan497@gmail.com>
2024-07-13 20:01:31 +0300

    added GET request

--- src/main/java/com/mycode/Main.java
+++ src/main/java/com/mycode/Main.java
@@ -2,10 +2,30 @@ package com.mycode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @GetMapping("/greet")
+    public GreetResponse greet(@RequestParam(value = "name", required = false) String name) {
+        String greetMessage = name == null || name.isEmpty() ? "Hello" : "Hello " + name;
+        return new GreetResponse(
+                greetMessage,
+                List.of("Java", "Golang", "Javascript"),
+                new Person("Alex", 28, 30_000));
+    }
+    record Person(String name, int age, double savings) {}
+    record GreetResponse(
+            String greet,
+            List<String> favProgLang,
+            Person person) {}
 }
